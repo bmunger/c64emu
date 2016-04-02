7a088219094632be4386ad65dc533babacbaed70
Stephen Stair <sgstair@akkit.org>
2016-04-01 21:08:39 -0700

    Early CPU emulation.

--- src/Cpu.cpp
+++ src/Cpu.cpp
@@ -19,10 +19,10 @@ void Cpu::Reset()
 {
 	A = 0;
 	P = 0;
-	S = 0;
+	S = 0xFF;
 	X = 0;
 	Y = 0;
-	PC = (AttachedMemory->Read8(0xFFFC) << 8) | AttachedMemory->Read8(0xFFFD);
+	PC = (AttachedMemory->Read8(0xFFFC)) | (AttachedMemory->Read8(0xFFFD) << 8);
 }
 
 bool Cpu::Step()
@@ -30,14 +30,177 @@ bool Cpu::Step()
 	unsigned short instructionPC;
 	instructionPC = PC;
 	unsigned char instruction;
+	unsigned char temp, temp2;
 
-	instruction = AttachedMemory->Read8(PC);
+	instruction = LoadInstructionByte(); 
+
+	switch (instruction)
+	{
+	// 0x00 row (Implied/Immediate)
+	case 0x20: TRACE_INSTRUCTION("JSR");
+		temp = LoadInstructionByte();
+		Push(High(PC));
+		Push(Low(PC));
+		temp2 = LoadInstructionByte();
+		SetLow(PC, temp); // Can't modify PC until after we load all the bytes for the instruction.
+		SetHigh(PC, temp2);
+		break;
+
+	// 0x01 row (Indirect, X)
+
+	case 0x01: TRACE_INSTRUCTION("ORA");
+		A = A | LoadInstructionByte();
+		SetResultFlags(A);
+		break;
+
+	// 0x02 row (Immediate)
+
+	case 0xA2: TRACE_INSTRUCTION("LDX"); // Load X (from immediate)
+		X = LoadInstructionByte();
+		SetResultFlags(X);
+		break;
+
+
+	// 0x08 row
+
+	case 0x08: TRACE_INSTRUCTION("PHP"); // Push P
+		Push(P | BFlag); // B flag is always set when pushing.
+		break;
+
+	case 0x28: TRACE_INSTRUCTION("PLP"); // Pull P
+		P = Pop() | OneFlag;
+		break;
+
+	case 0x48: TRACE_INSTRUCTION("PHA"); // Push A
+		Push(A);
+		break;
+
+	case 0x68: TRACE_INSTRUCTION("PLA"); // Pull A
+		A = Pop();
+		SetResultFlags(A);
+		break;
+
+	// 0x09 row
+
+	case 0x89: TRACE_INSTRUCTION("NOP"); // Undocumented NOP
+		break;
+
+
+
+
+	// 0x18 row
+	case 0x18: TRACE_INSTRUCTION("CLC");
+		ClearFlag(CFlag);
+		break;
+	case 0x38: TRACE_INSTRUCTION("SEC");
+		SetFlag(CFlag);
+		break;
+
+	case 0x58: TRACE_INSTRUCTION("CLI");
+		ClearFlag(IFlag);
+		break;
+
+
+	case 0x78: TRACE_INSTRUCTION("SEI");
+		SetFlag(IFlag);
+		break;
+
+	case 0xB8: TRACE_INSTRUCTION("CLV");
+		ClearFlag(VFlag);
+		break;
+
+
+	case 0xD8: TRACE_INSTRUCTION("CLD");
+		ClearFlag(DFlag);
+		break;
+	case 0xF8: TRACE_INSTRUCTION("SED");
+		SetFlag(DFlag);
+		break;
+
+
+	// 0x1A row 
+
+	case 0x9A: TRACE_INSTRUCTION("TXS"); // Transfer X to Stack register
+		S = X;
+		break;
+
+	// 0x1D row (Absolute, X)
+
+	case 0xBD: TRACE_INSTRUCTION("LDA"); // Load a from [x]
+		A = Load(X);
+		SetResultFlags(A);
+		break;
+
+
+	default:
+		TRACE_INSTRUCTION("Unrecognized Instruction");
+		return false; // CPU does not recognize this instruction.
+
+	}
+
+
+
+
+	return true;
+}
+
+unsigned char Cpu::Load(unsigned short Address)
+{
+	return AttachedMemory->Read8(Address);
+}
+
+// Set N/Z based on result.
+void Cpu::SetResultFlags(unsigned char Result)
+{
+	// Set Zero and Negative flags.
+	P = P & ~(ZFlag | NFlag);
+	P |= (Result & 0x80); // N flag
+	if (Result == 0)
+	{
+		P |= ZFlag;
+	}
+}
+
+void Cpu::SetFlag(unsigned char Flag)
+{
+	P = P | Flag;
+}
+void Cpu::ClearFlag(unsigned char Flag)
+{
+	P = P & (~Flag);
+}
+
+unsigned char Cpu::Low(unsigned short Reg)
+{
+	return Reg & 0xFF;
+}
+unsigned char Cpu::High(unsigned short Reg)
+{
+	return (Reg >> 8) & 0xFF;
+}
+void Cpu::SetLow(unsigned short & Reg, unsigned char NewLow)
+{
+	Reg = (Reg & 0xFF00) | NewLow;
+}
+void Cpu::SetHigh(unsigned short & Reg, unsigned char NewHigh)
+{
+	Reg = (Reg & 0x00FF) | ((unsigned short)NewHigh << 8);
+}
+
+void Cpu::Push(unsigned char Value)
+{
+	AttachedMemory->Write8(0x100 | S, Value);
+	S--;
+}
+unsigned char Cpu::Pop()
+{
+	S++;
+	return AttachedMemory->Read8(0x100 | S);
+}
+
+unsigned char Cpu::LoadInstructionByte()
+{
+	unsigned char byte = AttachedMemory->Read8(PC);
 	PC++;
-
-
-
-
-
-	TRACE_INSTRUCTION("Unrecognized Instruction");
-	return false; // CPU does not recognize this instruction.
+	return byte;
 }
--- src/Cpu.h
+++ src/Cpu.h
@@ -23,6 +23,34 @@ protected:
 	unsigned char X; // Index register X
 	unsigned char Y; // Index register Y
 
+	unsigned char Load(unsigned short Address);
+
+	void SetResultFlags(unsigned char Result);
+
+	void SetFlag(unsigned char Flag);
+	void ClearFlag(unsigned char Flag);
+
+	unsigned char Low(unsigned short Reg);
+	unsigned char High(unsigned short Reg);
+	void SetLow(unsigned short & Reg, unsigned char NewLow);
+	void SetHigh(unsigned short & Reg, unsigned char NewHigh);
+
+	void Push(unsigned char Value);
+	unsigned char Pop();
+
+	unsigned char LoadInstructionByte();
+
+
+	// Bit flags
+	const unsigned char NFlag = 0x80; // Negative. Set to the top bit of the result of an operation.
+	const unsigned char VFlag = 0x40; // Overflow. Overflow is when the carry into the top bit != the carry out of the top bit (occurs when the resulting math operation wraps around)
+	const unsigned char OneFlag = 0x20;// 0x20 flag is always 1
+	const unsigned char BFlag = 0x10; // Break. Always set except when the P register is being pushed on stack to service a hardware interrupt (to distiguish from BRK instruction interrupts)
+	const unsigned char DFlag = 0x08; // Decimal Mode. When 1, add/sub will use BCD mode. Annoying to emulate :)
+	const unsigned char IFlag = 0x04; // Interrupt Disable. Set to 1 to prevent hardware interrupts.
+	const unsigned char ZFlag = 0x02; // Zero. Set to 1 only when the result of the last operation was zero
+	const unsigned char CFlag = 0x01; // Carry. The raw carry out of add/sub operations, or the ejected bit in bit shifts.
+
 };
 
 #endif
